02e1c65fcb32d86bea1bc629c20edfa60a82d72c
zivmizrahi <zivm21@gmail.com>
2025-04-16 01:57:04 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
-// app/layout.tsx
-
+// app/layout.jsx
 import "./globals.css";
 
 export const metadata = {
@@ -7,11 +6,7 @@ export const metadata = {
   description: "Read the Bible in Hebrew and English",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
